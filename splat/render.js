28b25e00abf1eee0e02843b18f945b2b7c5a114a
dong <459188369@qq.com>
2024-09-04 18:50:21 +0800

    update

--- splat/render.js
+++ splat/render.js
@@ -61,86 +61,6 @@ function multiply4(a, b) {
     ];
 }
 
-function invert4(a) {
-    let b00 = a[0] * a[5] - a[1] * a[4];
-    let b01 = a[0] * a[6] - a[2] * a[4];
-    let b02 = a[0] * a[7] - a[3] * a[4];
-    let b03 = a[1] * a[6] - a[2] * a[5];
-    let b04 = a[1] * a[7] - a[3] * a[5];
-    let b05 = a[2] * a[7] - a[3] * a[6];
-    let b06 = a[8] * a[13] - a[9] * a[12];
-    let b07 = a[8] * a[14] - a[10] * a[12];
-    let b08 = a[8] * a[15] - a[11] * a[12];
-    let b09 = a[9] * a[14] - a[10] * a[13];
-    let b10 = a[9] * a[15] - a[11] * a[13];
-    let b11 = a[10] * a[15] - a[11] * a[14];
-    let det =
-        b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
-    if (!det) return null;
-    return [
-        (a[5] * b11 - a[6] * b10 + a[7] * b09) / det,
-        (a[2] * b10 - a[1] * b11 - a[3] * b09) / det,
-        (a[13] * b05 - a[14] * b04 + a[15] * b03) / det,
-        (a[10] * b04 - a[9] * b05 - a[11] * b03) / det,
-        (a[6] * b08 - a[4] * b11 - a[7] * b07) / det,
-        (a[0] * b11 - a[2] * b08 + a[3] * b07) / det,
-        (a[14] * b02 - a[12] * b05 - a[15] * b01) / det,
-        (a[8] * b05 - a[10] * b02 + a[11] * b01) / det,
-        (a[4] * b10 - a[5] * b08 + a[7] * b06) / det,
-        (a[1] * b08 - a[0] * b10 - a[3] * b06) / det,
-        (a[12] * b04 - a[13] * b02 + a[15] * b00) / det,
-        (a[9] * b02 - a[8] * b04 - a[11] * b00) / det,
-        (a[5] * b07 - a[4] * b09 - a[6] * b06) / det,
-        (a[0] * b09 - a[1] * b07 + a[2] * b06) / det,
-        (a[13] * b01 - a[12] * b03 - a[14] * b00) / det,
-        (a[8] * b03 - a[9] * b01 + a[10] * b00) / det,
-    ];
-}
-
-function rotate4(a, rad, x, y, z) {
-    let len = Math.hypot(x, y, z);
-    x /= len;
-    y /= len;
-    z /= len;
-    let s = Math.sin(rad);
-    let c = Math.cos(rad);
-    let t = 1 - c;
-    let b00 = x * x * t + c;
-    let b01 = y * x * t + z * s;
-    let b02 = z * x * t - y * s;
-    let b10 = x * y * t - z * s;
-    let b11 = y * y * t + c;
-    let b12 = z * y * t + x * s;
-    let b20 = x * z * t + y * s;
-    let b21 = y * z * t - x * s;
-    let b22 = z * z * t + c;
-    return [
-        a[0] * b00 + a[4] * b01 + a[8] * b02,
-        a[1] * b00 + a[5] * b01 + a[9] * b02,
-        a[2] * b00 + a[6] * b01 + a[10] * b02,
-        a[3] * b00 + a[7] * b01 + a[11] * b02,
-        a[0] * b10 + a[4] * b11 + a[8] * b12,
-        a[1] * b10 + a[5] * b11 + a[9] * b12,
-        a[2] * b10 + a[6] * b11 + a[10] * b12,
-        a[3] * b10 + a[7] * b11 + a[11] * b12,
-        a[0] * b20 + a[4] * b21 + a[8] * b22,
-        a[1] * b20 + a[5] * b21 + a[9] * b22,
-        a[2] * b20 + a[6] * b21 + a[10] * b22,
-        a[3] * b20 + a[7] * b21 + a[11] * b22,
-        ...a.slice(12, 16),
-    ];
-}
-
-function translate4(a, x, y, z) {
-    return [
-        ...a.slice(0, 12),
-        a[0] * x + a[4] * y + a[8] * z + a[12],
-        a[1] * x + a[5] * y + a[9] * z + a[13],
-        a[2] * x + a[6] * y + a[10] * z + a[14],
-        a[3] * x + a[7] * y + a[11] * z + a[15],
-    ];
-}
-
 function createWorker(self) {
     let buffer;
     let vertexCount = 0;
@@ -207,27 +127,27 @@ function createWorker(self) {
         // load it into webgl.
         for (let i = 0; i < vertexCount; i++) {
             // x, y, z
-            texdata_f[8 * i + 0] = f_buffer[8 * i + 0];
-            texdata_f[8 * i + 1] = f_buffer[8 * i + 1];
-            texdata_f[8 * i + 2] = f_buffer[8 * i + 2];
+            texdata_f[8 * i + 0] = f_buffer[14 * i + 0];
+            texdata_f[8 * i + 1] = f_buffer[14 * i + 1];
+            texdata_f[8 * i + 2] = f_buffer[14 * i + 2];
 
             // r, g, b, a
-            texdata_c[4 * (8 * i + 7) + 0] = u_buffer[32 * i + 24 + 0];
-            texdata_c[4 * (8 * i + 7) + 1] = u_buffer[32 * i + 24 + 1];
-            texdata_c[4 * (8 * i + 7) + 2] = u_buffer[32 * i + 24 + 2];
-            texdata_c[4 * (8 * i + 7) + 3] = u_buffer[32 * i + 24 + 3];
+            texdata_c[4 * (8 * i + 7) + 0] = f_buffer[14 * i + 6 + 0];
+            texdata_c[4 * (8 * i + 7) + 1] = f_buffer[14 * i + 6 + 1];
+            texdata_c[4 * (8 * i + 7) + 2] = f_buffer[14 * i + 6 + 2];
+            texdata_c[4 * (8 * i + 7) + 3] = f_buffer[14 * i + 6 + 3];
 
             // quaternions
             let scale = [
-                f_buffer[8 * i + 3 + 0],
-                f_buffer[8 * i + 3 + 1],
-                f_buffer[8 * i + 3 + 2],
+                f_buffer[14 * i + 3 + 0],
+                f_buffer[14 * i + 3 + 1],
+                f_buffer[14 * i + 3 + 2],
             ];
             let rot = [
-                (u_buffer[32 * i + 28 + 0] - 128) / 128,
-                (u_buffer[32 * i + 28 + 1] - 128) / 128,
-                (u_buffer[32 * i + 28 + 2] - 128) / 128,
-                (u_buffer[32 * i + 28 + 3] - 128) / 128,
+                f_buffer[14 * i + 10 + 0],
+                f_buffer[14 * i + 10 + 1],
+                f_buffer[14 * i + 10 + 2],
+                f_buffer[14 * i + 10 + 3],
             ];
 
             // Compute the matrix product of S and R (M = S * R)
@@ -285,9 +205,9 @@ function createWorker(self) {
         let sizeList = new Int32Array(vertexCount);
         for (let i = 0; i < vertexCount; i++) {
             let depth =
-                ((viewProj[2] * f_buffer[8 * i + 0] +
-                    viewProj[6] * f_buffer[8 * i + 1] +
-                    viewProj[10] * f_buffer[8 * i + 2]) *
+                ((viewProj[2] * f_buffer[14 * i + 0] +
+                    viewProj[6] * f_buffer[14 * i + 1] +
+                    viewProj[10] * f_buffer[14 * i + 2]) *
                     4096) |
                 0;
             sizeList[i] = depth;
@@ -582,13 +502,11 @@ async function main() {
             avgFps = avgFps * 0.9 + currentFps * 0.1;
     
             if (vertexCount > 0) {
-                document.getElementById("spinner").style.display = "none";
                 gl.uniformMatrix4fv(u_view, false, viewMatrix);
                 gl.clear(gl.COLOR_BUFFER_BIT);
                 gl.drawArraysInstanced(gl.TRIANGLE_FAN, 0, 4, vertexCount);
             } else {
                 gl.clear(gl.COLOR_BUFFER_BIT);
-                document.getElementById("spinner").style.display = "";
             }
             fps.innerText = Math.round(avgFps) + " fps";
             lastFrame = now;
@@ -598,13 +516,19 @@ async function main() {
     
     setInterval(frame, frameInterval);
 
-    const rowLength = 3 * 4 + 3 * 4 + 4 + 4;
+    const model = await tf.loadGraphModel('http://10.10.22.222:8000/outtfjs/model.json');
+
+    const rowLength = 3 + 3 + 4 + 4;
     const socketUrl = "ws://10.10.22.222:8001";
     const ws = new WebSocket(socketUrl);
 
     ws.onmessage = async (event) => {
-        const splatData = new Uint8Array(await event.data.arrayBuffer());
-        console.error('length: ', splatData.length);
+        let coeff = new Float32Array(await event.data.arrayBuffer());
+        const output = model.predict(tf.tensor(coeff));
+
+        splatData = new Float32Array(output.dataSync());
+
+        console.log('length: ', splatData.length, splatData.length / rowLength);
         worker.postMessage({
             buffer: splatData.buffer,
             vertexCount: Math.floor(splatData.length / rowLength),
@@ -613,6 +537,5 @@ async function main() {
 }
 
 main().catch((err) => {
-    document.getElementById("spinner").style.display = "none";
     document.getElementById("message").innerText = err.toString();
 });
---
+++ splat/server.py
@@ -0,0 +1,34 @@
+import asyncio
+import websockets
+import numpy as np
+import time
+
+flame_param = np.load(r'E:\workplace\AvatarSplat\GaussianAvatars\output\flame_param.npz')
+
+async def send_file(websocket, path):
+    fps = 30
+
+    try:
+        buffer_length = 418
+        float32_array = np.zeros(buffer_length, dtype=np.float32)  # Example data
+        i = 0
+        # Step 3: Send the buffer over the WebSocket connection
+        while(True):
+            float32_array = np.concatenate([np.zeros([300]), flame_param['expr'][i], np.zeros([3]), flame_param['neck_pose'][i], flame_param['jaw_pose'][i], flame_param['eyes_pose'][i], flame_param['translation'][i]], dtype=np.float32)
+            print(i)
+            await websocket.send(float32_array.tobytes())
+            await asyncio.sleep(0.1)
+            i += 1
+            if(i>72):
+                i = 0
+
+    except websockets.exceptions.ConnectionClosed:
+        print("Client disconnected")
+
+async def main():
+    async with websockets.serve(send_file, "0.0.0.0", 8001):
+        print("Server started on ws://0.0.0.0:8001")
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
